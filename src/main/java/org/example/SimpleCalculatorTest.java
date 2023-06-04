ca141a1e4d5344373206d6534a858fb1a7578bd1
sksjw95 <tjwldnd95@hanmail.net>
2023-06-04 19:33:08 +0900

    calculator

---
+++ src/main/java/org/example/SimpleCalculatorTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SimpleCalculatorTest {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요.");
+        String str1 = scan.nextLine();
+
+        System.out.println("사칙연산자를 입력하세요");
+        String op = scan.nextLine();
+
+        System.out.println("두 번째를 입력하세요");
+        String str2 = scan.nextLine();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        int result;
+
+        if(op.equals("+")){
+            result = num1 + num2 ;
+        }
+        else if(op.equals("-")){
+            result = num1 - num2 ;
+        }
+        else if (op.equals("/")){
+            result = num1 / num2;
+        }
+        else{
+            result = num1 * num2;
+        }
+        System.out.println(str1 + " " + op + " " + str2 + "=" + result);
+    }
+}
